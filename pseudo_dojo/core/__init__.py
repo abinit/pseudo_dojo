e199878634da8538dc6858d376c04f5942247a60
gmatteo <gmatteo@gmail.com>
2014-07-20 01:04:44 +0200

    Start to rationalize dependencies

--- pseudo_dojo/core/__init__.py
+++ pseudo_dojo/core/__init__.py
@@ -1,6 +1,4 @@
-from pymatgen.io.abinitio.tasks import TaskManager
-from pymatgen.io.abinitio.pseudos import PseudoParser, PseudoTable
-
 from .testing import *
 from .atom import *
-
+#from pymatgen.io.abinitio.tasks import TaskManager
+#from pymatgen.io.abinitio.pseudos import PseudoParser, PseudoTable
