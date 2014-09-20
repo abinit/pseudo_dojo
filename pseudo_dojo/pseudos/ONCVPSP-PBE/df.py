7af375512f49fe1fd65dbe04d552679038a60550
gmatteo <gmatteo@gmail.com>
2014-09-20 17:08:28 +0200

    Tables in RST

--- pseudo_dojo/pseudos/ONCVPSP-PBE/df.py
+++ pseudo_dojo/pseudos/ONCVPSP-PBE/df.py
@@ -5,6 +5,7 @@ import sys
 import os
 import pandas as pd
 
+from tabulate import tabulate
 from collections import OrderedDict, namedtuple
 from pprint import pprint
 from pymatgen.io.abinitio import Pseudo
@@ -74,18 +75,20 @@ def main():
         + [acc + "_ecut" for acc in accuracies]
     ]
 
-    print(" ONCVPSP TABLE ".center(80, "="))
+    print("\nONCVPSP TABLE:\n") #.center(80, "="))
     columns = [acc + "_dfact_meV" for acc in accuracies] 
     columns += [acc + "_ecut" for acc in accuracies] 
+    #print(data.to_string(columns=columns))
+    print(tabulate(data[columns], headers="keys", tablefmt="rst"))
 
-    print(data.to_string(columns=columns))
-
-    print(" STATS ".center(80, "="))
-    print(data.describe())
+    print("\nSTATS:\n") #.center(80, "="))
+    #print(data.describe())
+    print(tabulate(data.describe(), headers="keys", tablefmt="rst"))
 
     bad = data[data["high_dfact_meV"] > data["high_dfact_meV"].mean()]
-    print(" BAD PSEUDOS (high_dfact > mean) ".center(80, "*"))
-    print(bad)
+    print("\nPSEUDOS with high_dfact > mean:\n") # ".center(80, "*"))
+    #print(bad)
+    print(tabulate(bad, headers="keys", tablefmt="rst"))
 
     if errors:
         print("ERRORS:")
