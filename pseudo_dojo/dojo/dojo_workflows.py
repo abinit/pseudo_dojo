3de99632da0d6bc7a427edf1013b692cff6f5727
setten <setten@gmail.com>
2014-10-30 14:19:21 +0100

    Phonon Dojo training

--- pseudo_dojo/dojo/dojo_workflows.py
+++ pseudo_dojo/dojo/dojo_workflows.py
@@ -844,10 +844,11 @@ class DFPTPhonoFactory(object):
 
         kwargs.pop('accuracy')
         kwargs.pop('smearing')
-        print(kwargs)
 
         global_vars = dict(ksampling.to_abivars(), nband=4, ecut=3.0, tolvrs=1.0e-8, paral_kgb=0)
         global_vars.update(**kwargs)
+
+        print(global_vars)
 
         inp = abilab.AbiInput(pseudos=pseudos, ndtset=1+len(qpoints))
 
