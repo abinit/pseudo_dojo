4eecdb391d1745ae16c4dd9e4236af65df1f0bfc
gmatteo <gmatteo@gmail.com>
2015-01-17 03:42:48 +0100

    prof option for dojorun

--- pseudo_dojo/scripts/dojorun.py
+++ pseudo_dojo/scripts/dojorun.py
@@ -159,4 +159,17 @@ Usage Example:\n
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    do_prof = False
+    try:
+        do_prof = sys.argv[1] == "prof"
+        if do_prof sys.argv.pop(1)
+    except: 
+        pass
+
+    if do_prof:
+        import pstats, cProfile
+        cProfile.runctx("main()", globals(), locals(), "Profile.prof")
+        s = pstats.Stats("Profile.prof")
+        s.strip_dirs().sort_stats("time").print_stats()
+    else:
+        sys.exit(main())
