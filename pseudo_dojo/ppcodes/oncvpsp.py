85d07e8374155e74eec6219b97a09ac87c6025b0
Michiel van Settn <mjvansetten@gmail.com>
2015-11-27 13:21:51 +0100

    LDA table mostly done
    PBE version 0.3
    added normal potentials for the high version
    remakes of potential that were still ONCVPSP 2.0.0

--- pseudo_dojo/ppcodes/oncvpsp.py
+++ pseudo_dojo/ppcodes/oncvpsp.py
@@ -968,7 +968,7 @@ class OncvOutputParser(PseudoGenOutputParser):
 
         if devel:
             # devellopment run: few, relatively high ecut calculations
-            d["ecuts"] = [estart, estart + 10]
+            d["ecuts"] = [estart, estart + 2]
 
         ps_data += "\n<DOJO_REPORT>\n" + json.dumps(d, indent=4) + "\n</DOJO_REPORT>\n"
 
