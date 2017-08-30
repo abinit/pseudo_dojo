f28631c3e029f3586bb7f75985f7ecd7949374e5
Michiel van Setten <mjvansetten@gmail.com>
2017-08-30 11:42:16 +0200

    some further improvements

--- website/dojo-tools.js
+++ website/dojo-tools.js
@@ -157,3 +157,11 @@ function product(x,y) {
     var r = document.getElementById('result-1');
     r.innerHTML = 'the product of ' + x + ' and ' + y + ' is ' + p
 }
+
+function humanize(size) {
+	var units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+	var ord = Math.floor(Math.log(size) / Math.log(1024));
+	ord = Math.min(Math.max(0, ord), units.length - 1);
+	var s = Math.round((size / Math.pow(1024, ord)) * 100) / 100;
+	return s + ' ' + units[ord];
+}
