77820ed725cc807824ad197ba622c6854749fa2a
Michiel van Setten <mjvansetten@googlemail.com>
2018-05-09 11:10:35 +0200

    activating the fr pbe stringent table

--- website/js/dojo-tools.js
+++ website/js/dojo-tools.js
@@ -330,7 +330,7 @@ function dynamicdropdown(listindex){
     case "nc-fr-04" :
       document.getElementById('warning_box').innerHTML = "";
       document.getElementById("ACC").options[0]=new Option("standard","standard");
-//      document.getElementById("ACC").options[1]=new Option("stringent","stringent");
+      document.getElementById("ACC").options[1]=new Option("stringent","stringent");
 //      document.getElementById("XCF").options[2]=new Option("LDA","pw");
       document.getElementById("XCF").options[0]=new Option("PBE","pbe");
 //      document.getElementById("XCF").options[1]=new Option("PBEsol","pbesol");
